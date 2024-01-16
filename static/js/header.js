80db1ab7b779132c4def0c3193a4d063c2ef96b2
tobitoast4 <tobi-30@gmx.de>
2024-01-16 22:44:00 +0100

    Back to menu overlay

--- static/js/header.js
+++ static/js/header.js
@@ -4,5 +4,8 @@ function updatePlayersTurnInstuction(player_name){
 }
 
 function updatePlayerWonTheGame(player_name){
-    $('#players_turn').html("Congratiulations! " + player_name + " won the game!");
+    $('#players_turn').html("" + player_name + " won the game!");
+
+    $('#back_to_menu_overlay').removeAttr("style");
+    $('#back_to_menu_overlay_text').text("" + player_name + " did win the game!");
 }
