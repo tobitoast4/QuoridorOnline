ebbf9ec6a9b9157e072c3c907f96105fc34a79fd
tobitoast4 <76886033+tobitoast4@users.noreply.github.com>
2026-06-26 23:14:54 +0200

    Update settings.py

--- conf/settings.py
+++ conf/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'conf.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.getenv("DJANGO_SQLITE_PATH", BASE_DIR / 'db.sqlite3'),
     }
 }
 
