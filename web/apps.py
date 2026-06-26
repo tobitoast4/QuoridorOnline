0b1b355c5e8199cca600ec6748cb8825a9c29fbb
tobitoast4 <76886033+tobitoast4@users.noreply.github.com>
2026-06-26 23:04:55 +0200

    Cleanup

--- web/apps.py
+++ web/apps.py
@@ -10,16 +10,4 @@ class WebConfig(AppConfig):
 
     def ready(self):
         pass
-        from . import models
-        from web.utils import create_lobby_from_json
-
-        return
-        for datei in Path("old_games").iterdir():
-            if datei.is_file():
-                # try:
-                    with open(datei, "r", encoding="utf-8") as f:
-                        inhalt = f.read()
-
-                    create_lobby_from_json(json.loads(inhalt))
-                # except Exception as e:
-                #     print(f"Fehler beim Verarbeiten der Datei {datei}: {e}")
+        # from . import models
