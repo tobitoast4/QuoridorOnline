793ac0bfe0adb9a005c7075ed34672ff23587eaf
tobitoast4 <76886033+tobitoast4@users.noreply.github.com>
2026-06-26 16:26:25 +0200

    Use environment variables

--- conf/settings.py
+++ conf/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0995xe=bdbtetfxvb*)!n1uan888mre!$#9say%xlar7+nxvt&'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "django-insecure-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DJANGO_DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = ['*']
 
