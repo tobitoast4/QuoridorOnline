65290a85dafd8841496dab4dca8d216cebd58934
tobitoast4 <76886033+tobitoast4@users.noreply.github.com>
2026-06-28 22:53:37 +0200

    Minor update

--- conf/settings.py
+++ conf/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # "web.errors.ErrorProcessingMiddleware",
     'web.middleware.AnonymousGameUserMiddleware',
 ]
 
--- web/errors.py
+++ web/errors.py
@@ -40,41 +40,41 @@ def custom_exception_handler(exc, context):
     return response
 
 
-# class ErrorProcessingMiddleware:
-#     # Middleware for setting hooks into Django’s request/response processing
-#     # See https://docs.djangoproject.com/en/3.0/topics/http/middleware/
-#     def __init__(self, get_response):
-#         self.get_response = get_response
-#         # One-time configuration and initialization.
+class ErrorProcessingMiddleware:
+    # Middleware for setting hooks into Django’s request/response processing
+    # See https://docs.djangoproject.com/en/3.0/topics/http/middleware/
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # One-time configuration and initialization.
 
-#     def __call__(self, request):
-#         # Code to be executed for each request before
-#         # the view (and later middleware) are called.
-#         response = self.get_response(request)
-#         # Code to be executed for each request/response after
-#         # the view is called.
-#         if response.status_code == 404:
-#             if (
-#                 hasattr(response, "path")
-#                 and ".well-known/appspecific/com.chrome.devtools.json" in response.path
-#             ):
-#                 # Path requested by Chrome. If we dont pass the response here, 404 appears often in the Notify.
-#                 return response
-#             messages.add_message(
-#                 request,
-#                 messages.ERROR,
-#                 "The requested resource was not found on this server.",
-#                 extra_tags="404 Not Found",
-#             )
-#             return redirect("home")
-#         return response
+    def __call__(self, request):
+        # Code to be executed for each request before
+        # the view (and later middleware) are called.
+        response = self.get_response(request)
+        # Code to be executed for each request/response after
+        # the view is called.
+        if response.status_code == 404:
+            if (
+                hasattr(response, "path")
+                and ".well-known/appspecific/com.chrome.devtools.json" in response.path
+            ):
+                # Path requested by Chrome. If we dont pass the response here, 404 appears often in the Notify.
+                return response
+            messages.add_message(
+                request,
+                messages.ERROR,
+                "The requested resource was not found on this server.",
+                extra_tags="404 Not Found",
+            )
+            return redirect("home")
+        return response
 
-#     def process_exception(self, request, exception):
-#         # Process exceptions in requests.
-#         # See https://docs.djangoproject.com/en/3.0/topics/http/middleware/#process-exception
-#         messages.add_message(
-#             request, messages.ERROR, exception, extra_tags=str(type(exception).__name__)
-#         )
-#         if "HTTP_REFERER" in request.META:
-#             return redirect(request.META["HTTP_REFERER"])
-#         return redirect("home")
+    def process_exception(self, request, exception):
+        # Process exceptions in requests.
+        # See https://docs.djangoproject.com/en/3.0/topics/http/middleware/#process-exception
+        messages.add_message(
+            request, messages.ERROR, exception, extra_tags=str(type(exception).__name__)
+        )
+        if "HTTP_REFERER" in request.META:
+            return redirect(request.META["HTTP_REFERER"])
+        return redirect("home")
