e8963b66ca62a605c70b37f0f084e01769e444c4
tobitoast4 <76886033+tobitoast4@users.noreply.github.com>
2026-07-03 10:51:41 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,4 @@
 #!/usr/bin/env bash
-# Exit on error
-set -o errexit
-
-# Modify this line as needed for your package manager (pip, poetry, etc.)
-pip install -r requirements.txt
-
-# Convert static asset files
 python manage.py collectstatic --no-input
-
-# Apply any outstanding database migrations
-python manage.py migrate
-
+# python manage.py migrate
 daphne -b 0.0.0.0 -p 8000 conf.asgi:application
