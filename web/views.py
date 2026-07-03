1cceb6caedc18e00d2a179a4c2ba2e087f850b6c
tobitoast4 <76886033+tobitoast4@users.noreply.github.com>
2026-07-03 13:58:07 +0200

    Remove old views

--- web/views.py
+++ web/views.py
@@ -147,85 +147,16 @@ def lobby(request, lobby_id=None):
         return render(request, "lobby.html", context)
 
 @api_view(['GET'])
-@permission_classes([IsAuthenticated])
-def get_lobby(request, lobby_id=None):
-    if lobby_id is None:
-        messages.add_message(request, messages.ERROR, f"The lobby with id {lobby_id} does not exist.")
-        return redirect("/")
-    the_lobbys = models.Lobby.objects.filter(pk=lobby_id)
-    if the_lobbys.count() == 1:
-        the_lobby = the_lobbys.first()
-        if the_lobby.game is None:  # game not started yet
-            lobby_manager.add_player_to_lobby(the_lobby, request.user)
-            lobby_manager.check_players_last_seen_time(the_lobby)
-            serializer = serialize.LobbySerializer(the_lobby)
-            return Response({"lobby": serializer.data}, 200)
-        else:  # game started
-            return Response({"lobby": {"game": f"game/{lobby_id}"}}, 200)  #  TODO: use redirect here
-    else:
-        messages.add_message(request, messages.ERROR, f"The lobby with id {lobby_id} does not exist.")
-        return redirect("/")
-
-@api_view(['GET'])
 def get_random_lobby(request):
     the_lobby = lobby_manager.get_random_public_lobby()
     if the_lobby is None:
         return Response({"error": "Could not find any public lobby :(<br/>Try again later or create your own one"}, 200)
     return Response({"lobby_url": f"/lobby/{the_lobby.id}"}, 200)
-    
-@api_view(['POST'])
-def start_game(request, lobby_id=None):
-    the_lobby = models.Lobby.objects.get(pk=lobby_id)
-    if request.user != the_lobby.owner.game_user:
-        return Response({"error": "Only the lobby owner can start the game"}, 200)
-    # TODO: Shuffle players
-    next_lobby = models.Lobby.objects.create(created_by=the_lobby.created_by, owner=the_lobby.owner)
-    new_game = quoridor_game.Game(the_lobby.gameplayer_set, the_lobby.amount_of_walls_per_player, next_lobby.id)
-    the_lobby.game = json.dumps(new_game.game_data, cls=utils.UUIDEncoder)
-    the_lobby.save()
-    return Response({"status": "game started"}, 200)
 
 def game(request, lobby_id=None):
     the_lobby = models.Lobby.objects.get(pk=lobby_id)
     context = {"user": request.user, "lobby": the_lobby}
     return render(request, "game_online.html", context)
-
-@api_view(['GET'])
-def get_game_data(request, lobby_id=None):
-    if lobby_id is None:
-        return Response({"error": "No lobby ID provided"}, 400)
-    else:
-        try:
-            the_lobby = models.Lobby.objects.get(id=lobby_id)
-        except models.Lobby.DoesNotExist:
-            return Response({"error": "Lobby not found"}, 404)
-        serializer = serialize.LobbySerializer(the_lobby)
-        json_data = serializer.data
-        return Response({"lobby": json_data}, 200)
-
-
-@api_view(['POST'])
-def change_lobby_visibility(request, lobby_id=None):
-    the_lobby = models.Lobby.objects.get(id=lobby_id)
-    the_lobby.toggle_visibility()
-    if the_lobby.is_private:
-        return Response({"status": "Lobby visibility successfully changed to: private"}, 200)
-    else:
-        return Response({"status": "Lobby visibility successfully changed to: public"}, 200)
-
-@api_view(['POST'])
-def change_amount_of_walls_per_player(request, lobby_id=None):
-    the_lobby = models.Lobby.objects.get(id=lobby_id)
-    if request.user.id != the_lobby.owner.game_user.id:
-        raise PermissionError("Only the lobby owner can change the amount of walls per player")
-    if the_lobby.game is not None:
-        raise PermissionError("You can not change the amount of walls per player when the game is already running")
-    new_amount = request.data.get("new_amount")
-    the_lobby.change_amount_of_walls_of_players(new_amount)
-    return Response({
-        "status": "ok",
-        "amount_of_walls_per_player": new_amount
-    }, 200)
 
 @api_view(['POST'])
 def rename_player(request):
