7af72dcaf35ff38598eb2214e3a37c87d2a43300
tobitoast4 <76886033+tobitoast4@users.noreply.github.com>
2026-07-03 16:04:56 +0200

    Update stats.js

--- web/static/js/stats.js
+++ web/static/js/stats.js
@@ -2,17 +2,22 @@ function refreshPlayerStats() {
     $('#stats_grid').empty();
     players.forEach(player => {
         var player_stats = $('<div class="two-col-grid"></div>');
-        player_stats.append('<h2 style="text-align: right;">' + player.name + `
-            <span id="online-status-${player.player_id}" title="online" class="online-dot"" style="background-color: #00DD00;"></span>
+        player_stats.append('<h2 style="text-align: right;">' + player.name + `<span 
+            id="online-status-${player.player_id}" title="online" class="online-dot" style="background-color: #a8a8a8;"></span>
         </h2>`);
     
-        var wall_box = $('<div id="remaining_walls_container" style="display: flex; gap: 3px; padding: 20px"></div>');
+        var wall_box = $('<div id="remaining_walls_container" style="display: flex; gap: 3px; padding: 20px; padding-left: 8px;"></div>');
         for (var i = 0; i < player.amount_walls_left; i++) {
             wall_box.append('<div style="background-color: ' + player.color + '; width: 5px; height: 24px;"></div>');
         }
         player_stats.append(wall_box);
         $('#stats_grid').append(player_stats);
+
+        if (player.player_id == this_player_id) {
+            updatePlayerOnlineStatus(this_player_id, true);
+        }
     });
+
 }
 
 function updatePlayerOnlineStatus(player_id, is_online) {
