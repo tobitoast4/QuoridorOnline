130552a87a2a70e7049ac3ed8100acfbe75f15bf
tobitoast4 <76886033+tobitoast4@users.noreply.github.com>
2026-07-10 16:03:50 +0200

    Start ASGI

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 python manage.py collectstatic --no-input
 # python manage.py migrate
 # daphne -b 0.0.0.0 -p 10000 conf.asgi:application
-gunicorn --bind 0.0.0.0:10000 conf.wsgi:application
+gunicorn --bind 0.0.0.0:10000 -k uvicorn.workers.UvicornWorker conf.asgi:application
