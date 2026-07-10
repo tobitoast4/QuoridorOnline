8bc39bf2ed72167cb8c92662b8e9ba505040acef
tobitoast4 <76886033+tobitoast4@users.noreply.github.com>
2026-07-10 16:31:51 +0200

    Use daphne again

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 python manage.py collectstatic --no-input
 # python manage.py migrate
-# daphne -b 0.0.0.0 -p 10000 conf.asgi:application
+daphne -b 0.0.0.0 -p 10000 conf.asgi:application
 # gunicorn --bind 0.0.0.0:10000 -k uvicorn.workers.UvicornWorker conf.asgi:application
-python -m gunicorn conf.asgi:application -k uvicorn.workers.UvicornWorker
+# python -m gunicorn conf.asgi:application -k uvicorn.workers.UvicornWorker
